21a8a7e1aeb6d4189526dab45d0e679aef720a26
Ritesh Chaudhary <ritesh.chaudhari.t5a17@icp.edu.np>
2021-01-12 13:09:48 +0545

    func

---
+++ Functions In Depth/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+///////////////////////////////////////
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
